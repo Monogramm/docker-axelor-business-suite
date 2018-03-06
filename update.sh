ebaa335d3c950dfa06facfd46f8df12139e3780a
madmath03 <mb.mathieu.brunot@gmail.com>
2018-03-06 11:48:24 +0100

    :sparkles: Introduction of tomcat alpine variant

--- update.sh
+++ update.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 set -eo pipefail
 
+declare -A pkg_install=(
+	[jre8]='apt-get update -q; apt-get install -y --no-install-recommends '
+	[jre8-alpine]='apk add --no-cache '
+)
+
+declare -A pkg_clean=(
+	[jre8]='rm -rf \/var\/lib\/apt\/lists\/*; '
+	[jre8-alpine]=''
+)
+
 # version_greater_or_equal A B returns whether A >= B
 function version_greater_or_equal() {
 	[[ "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1" || "$1" == "$2" ]];
 }
 
 
-dockerRepo="madmath03/docker-axelor-business-suite"
+dockerRepo="monogramm/docker-axelor-business-suite"
 # TODO Find a way to retrieve automatically the latest versions from GitHub
 # latests=( $( curl -fsSL 'https://api.github.com/repos/axelor/abs-webapp/tags' |tac|tac| \
 # 	grep -oE '[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+' | \
@@ -34,34 +44,39 @@ for latest in "${latests[@]}"; do
 	# Only add versions >= 4.0
 	if version_greater_or_equal "$version" "4.2"; then
 
-		echo "updating $latest [$version]"
+		for variant in jre8 jre8-alpine; do
+			echo "updating $latest [$version] $variant"
 
-		# Create the version directory with a Dockerfile.
-		dir="images/$version"
-		mkdir -p "$dir"
+			# Create the version directory with a Dockerfile.
+			dir="images/$version/$variant"
+			mkdir -p "$dir"
 
-		template="Dockerfile.template"
-		cp "$template" "$dir/Dockerfile"
+			template="Dockerfile.template"
+			cp "$template" "$dir/Dockerfile"
 
-		# Replace the variables.
-		sed -ri -e '
-			s/%%ADK_VERSION%%/'"$adkVersion"'/g;
-			s/%%ABS_VERSION%%/'"$latest"'/g;
-		' "$dir/Dockerfile"
+			# Replace the variables.
+			sed -ri -e '
+				s/%%VARIANT%%/'"$variant"'/g;
+				s/%%ADK_VERSION%%/'"$adkVersion"'/g;
+				s/%%ABS_VERSION%%/'"$latest"'/g;
+				s/%%PKG_INSTALL%%/'"${pkg_install[$variant]}"'/g;
+				s/%%PKG_CLEAN%%/'"${pkg_clean[$variant]}"'/g;
+			' "$dir/Dockerfile"
 
-		# Copy the shell scripts
-		for name in entrypoint; do
-			cp "docker-$name.sh" "$dir/$name.sh"
-			chmod 755 "$dir/$name.sh"
+			# Copy the shell scripts
+			for name in entrypoint; do
+				cp "docker-$name.sh" "$dir/$name.sh"
+				chmod 755 "$dir/$name.sh"
+			done
+
+			travisEnv='\n    - VERSION='"$version"' VARIANT='"$variant$travisEnv"
+
+			if [[ $1 == 'build' ]]; then
+				tag="$version-$variant"
+				echo "Build Dockerfile for ${tag}"
+				docker build -t ${dockerRepo}:${tag} $dir
+			fi
 		done
-
-		travisEnv='\n    - VERSION='"$version$travisEnv"
-
-		if [[ $1 == 'build' ]]; then
-			tag="$version"
-			echo "Build Dockerfile for ${tag}"
-			docker build -t ${dockerRepo}:${tag} $dir
-		fi
 	fi
 done
 
